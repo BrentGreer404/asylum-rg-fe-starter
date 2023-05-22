15c3b418dd033d49bdd9a3515fc8bc670cc0223c
BrentGreer404 <yem1546@yahoo.com>
2023-05-22 10:07:53 -0400

    fixed profilepage did not save

--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -1,8 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 
-export function UserPage() {
+export function ProfilePage() {
   const { user } = useAuth0();
-  console.log(user);
   const { name, email, picture } = user;
   return (
     <div style={{ margin: 'auto', padding: '5%' }}>
